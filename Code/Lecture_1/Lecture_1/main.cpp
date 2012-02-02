1f9b3d1cdbcceccb38a7bdd6a6547c36490e9a18
Nis Sarup <nis@sarup.dk>
2012-02-02 15:21:44 +0100

    Example is working!

--- Code/Lecture_1/Lecture_1/main.cpp
+++ Code/Lecture_1/Lecture_1/main.cpp
@@ -12,14 +12,40 @@
 
 int main (int argc, const char * argv[])
 {
-    const Int n = 4;
+    const Int n = 3;
     MatDoub a(n,n);
     VecDoub b(n),x(n);
     
+    a[0][0] = 2;
+    a[0][1] = 4;
+    a[0][2] = 8;
+    a[1][0] = 4;
+    a[1][1] = 8;
+    a[1][2] = 4;
+    a[2][0] = -2;
+    a[2][1] = 10;
+    a[2][2] = 5;
+    
+    b[0] = 1;
+    b[1] = 2;
+    b[2] = 3;
+    
+    
     LUdcmp alu(a);
     
+    alu.solve(b, x);
+    
+    std::cout << "x0 = " << x[0] << endl;
+    std::cout << "x1 = " << x[1] << endl;
+    std::cout << "x2 = " << x[2] << endl;
+    
+    
+    std::cout << "[ " << alu.lu[0][0] << " " << alu.lu[0][1] << " "  << alu.lu[0][2] << " "  << "]" << endl;
+    std::cout << "[ " << alu.lu[1][0] << " " << alu.lu[1][1] << " "  << alu.lu[1][2] << " "  << "]" << endl;
+    std::cout << "[ " << alu.lu[2][0] << " " << alu.lu[2][1] << " "  << alu.lu[2][2] << " "  << "]" << endl;
+    
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Done!\n";
     return 0;
 }
 
