84082a820591c81e757efa648ad68e44345df7f8
Nis Sarup <nis@sarup.dk>
2012-03-29 15:28:28 +0200

    Lecture 12 done.

---
+++ Code/Lektion 12/Lektion 12/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Lektion 12
+//
+//  Created by Nis Sarup on 29/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
